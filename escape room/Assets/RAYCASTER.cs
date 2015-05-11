e7f0664887547d02b575263531d4ea183ede4872
Rysiek <pocztadomnie@onet.pl>
2015-05-11 12:24:51 +0200

    no message

---
+++ escape room/Assets/RAYCASTER.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RAYCASTER : MonoBehaviour {
+
+	private RaycastHit hit;
+	private Collider poprzedniCol=null;//RAycast
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (poprzedniCol);
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		if (Physics.Raycast (transform.position, fwd, out hit)) {
+			if (hit.collider != poprzedniCol) {
+				if (poprzedniCol != null)
+					poprzedniCol.SendMessage ("Koniec", SendMessageOptions.DontRequireReceiver);
+				poprzedniCol = hit.collider;
+
+				if (poprzedniCol != null)
+					poprzedniCol.SendMessage ("Poczatek", SendMessageOptions.DontRequireReceiver);
+
+
+			} else {
+				poprzedniCol.SendMessage ("Ciagle", SendMessageOptions.DontRequireReceiver);
+			}
+		} else {
+			if (poprzedniCol != null)
+				poprzedniCol.SendMessage ("Koniec", SendMessageOptions.DontRequireReceiver);
+			poprzedniCol=null;
+		}
+
+
+	}
+}
---
+++ escape room/Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class półka : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Poczatek(){
+		transform.position = new Vector3(0,0,0)
+		
+	}
+	void Ciagle(){
+		Debug.Log ("ciagle");
+	}
+	void Koniec(){
+		Debug.Log ("koniec");
+	}
+}
